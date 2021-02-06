c0042c9dad55222d2f2d3e98cbc462861ad24bba
Enias Oliveira <eniasoliveira27@gmail.com>
2021-02-05 22:16:17 -0300

    refactor: creates custom hook to store user token

--- src/components/authenticator/index.jsx
+++ src/components/authenticator/index.jsx
@@ -1,71 +1,75 @@
-import { useEffect, useState } from "react";
-import { Route, Switch, useHistory } from "react-router-dom";
+import { Route, Switch } from "react-router-dom";
 
 import Login from "../../pages/login";
 import UserForm from "../../pages/userForm";
 import Users from "../../pages/users";
 import Feedbacks from "../../pages/feedbacks";
 import FeedbackForm from "../../pages/feedbackForm";
-import axios from "axios";
 
 import Container from "react-bootstrap/Container";
+import NavBar from "../navbar";
+import useToken from "./useToken";
 
 const Authenticator = () => {
-  const [isAuthenticated, setAuthentication] = useState(undefined);
-  const history = useHistory();
+  const { token, setToken } = useToken();
 
-  useEffect(() => {
-    const token = window.localStorage.getItem("authToken");
-    if (!token) {
-      setAuthentication(false);
-    } else {
-      axios
-        .get("https://ka-users-api.herokuapp.com/users", {
-          headers: { Authorization: token },
-        })
-        .then(() => {
-          setAuthentication(true);
-          history.push("/users");
-        })
-        .catch(() => {
-          setAuthentication(false);
-        });
-    }
-  }, [history, setAuthentication]);
-  if (isAuthenticated === undefined) {
-    return (
-      <Container
-        style={{
-          width: "100%",
-          height: "100%",
-          textAlign: "center",
-          paddingTop: "25vh",
-          color: "#fff",
-        }}
-      >
-        <h1>Carregando...</h1>
-      </Container>
-    );
+  if (!token) {
+    return <Login />;
   }
 
-  // Não tá redirecionando quando o usuário tá deslogado
-  if (isAuthenticated === false) {
-    return (
-      <Switch>
-        <Route exact path="/userform">
-          <UserForm />
-        </Route>
-        <Route exact path="/">
-          <Login setAuthentication={setAuthentication} />
-        </Route>
-      </Switch>
-    );
-  }
+  // useEffect(() => {
+  //   if (!token) {
+  //     setAuthentication(false);
+  //   } else {
+  //     axios
+  //       .get("https://ka-users-api.herokuapp.com/users", {
+  //         headers: { Authorization: token },
+  //       })
+  //       .then(() => {
+  //         setAuthentication(true);
+  //         history.push("/users");
+  //       })
+  //       .catch(() => {
+  //         setAuthentication(false);
+  //       });
+  //   }
+  // }, [history, setAuthentication]);
+
+  // if (isAuthenticated === undefined) {
+  //   return (
+  //     <Container
+  //       style={{
+  //         width: "100%",
+  //         height: "100%",
+  //         textAlign: "center",
+  //         paddingTop: "25vh",
+  //         color: "#fff",
+  //       }}
+  //     >
+  //       <h1>Carregando...</h1>
+  //     </Container>
+  //   );
+  // }
+
+  // // Não tá redirecionando quando o usuário tá deslogado
+  // if (isAuthenticated === false) {
+  //   return (
+  //     <Switch>
+  //       <Route exact path="/userform">
+  //         <UserForm />
+  //       </Route>
+  //       <Route exact path="/">
+  //         <Login setAuthentication={setAuthentication} />
+  //       </Route>
+  //     </Switch>
+  //   );
+  // }
 
   return (
     <Switch>
+      <NavBar />
       <Route exact path="/">
-        <Login setAuthentication={setAuthentication} />
+        <Login setAuthentication={setToken} />
       </Route>
       <Route exact path="/users">
         <Users />
